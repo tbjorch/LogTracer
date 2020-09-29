4a6164260fd7b67d12bfdef3bf51bdee0a4548b0
Tobias Björch <t.bjorch@gmail.com>
2020-09-29 20:59:00 +0200

    refactoring

--- logtracer.py
+++ logtracer.py
@@ -4,19 +4,19 @@ from functools import wraps
 import inspect
 
 
-class TraceUtil():
+class LogTracer():
     """
-    TraceUtil provides a decorator allowing detailed output of function
+    LogTracer provides a decorator allowing detailed output of function
     execution to facilitate tracing, while at the same time allowing masking
     of arguments by argument name.
 
-    On creation, TraceUtil takes varargs of argument names to be masked
+    On creation, LogTracer takes varargs of argument names to be masked
 
     Example ----------------------------------------------------------------
-    tu = TraceUtil("password", "pw")
+    lt = LogTracer("password", "pw")
     log = logging.getLogger(__name__)
 
-    @tu.trace(log)
+    @lt.trace(log)
     def login(user, pw, username=None, password=None):
         # function executing stuff
     ------------------------------------------------------------------------
@@ -28,10 +28,11 @@ class TraceUtil():
 
     2020-09-28 19:58:31,733 - my_module_name  - DEBUG - Executing function login with args=['Arnold', '**********'] and kwargs={'username': 'pony', 'password': '**********'}
     """
-    def __init__(self, *excluded_args: str):
-        self.excluded_args = excluded_args
+    def __init__(self, log: Logger, *masked_args: str):
+        self.masked_args = masked_args
+        self.log = log
 
-    def trace(self, log: Logger) -> Callable:
+    def trace(self) -> Callable:
         """
         Decorator method creating trace log.
 
@@ -46,7 +47,7 @@ class TraceUtil():
             @wraps(function)
             def inner_wrapper(*args, **kwargs):
                 arg_obj = inspect.getfullargspec(function)
-                log.debug(
+                self.log.debug(
                     f"Executing function {function.__name__} with "
                     f"{self.__create_arg_log_string(arg_obj, *args, **kwargs)}"
                     f" and {self.__create_kwarg_log_string(**kwargs)}"
@@ -56,21 +57,20 @@ class TraceUtil():
         return wrapper
 
     def __create_arg_log_string(self, arg_obj, *args, **kwargs) -> str:
-        if arg_obj.defaults is not None \
-                and len(arg_obj.args) == len(arg_obj.defaults) \
-                and len(args) == 0:
+        if len(args) == 0:
             return "args=[]"
         arg_list = list(args)
+        # Iterating through positional arguments and breaking when/if we
+        # reach default arguments, found in the kwarg parameter.
         for i, arg in enumerate(arg_obj.args):
-            if (arg_obj.defaults is not None) \
-                    and (i > len(arg_obj.defaults)-1):
+            if i > len(arg_list):
                 break
-            if arg in self.excluded_args:
+            if arg in self.masked_args:
                 arg_list[i] = "**********"
         return f"args={arg_list}"
 
     def __create_kwarg_log_string(self, **kwargs) -> str:
         for kwarg in kwargs:
-            if kwarg in self.excluded_args:
+            if kwarg in self.masked_args:
                 kwargs[kwarg] = "**********"
         return f"kwargs={kwargs}"
